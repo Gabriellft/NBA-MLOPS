6dec725211b504d1ef418afd1977e3b8074828f0
Gabriel Laffitte <gabriel.laffitte@efrei.net>
2024-02-03 17:52:21 +0100

    Add new version + data for record

--- mlops_nba/train.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-
